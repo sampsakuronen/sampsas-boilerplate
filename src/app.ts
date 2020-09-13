87c10ed2b773f98ffa7f59ac924719fc34a606d2
Sampsa Kuronen <sampsa.kuronen@gmail.com>
2020-09-13 10:18:18 +0300

    Add compression

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import {router} from 'typera-express';
 import * as helmet from 'helmet';
+import * as compression from 'compression';
 
 import {updateUser} from './routes/user';
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(compression());
 app.use(helmet({contentSecurityPolicy: false})); // TODO: Change the CSP to match your needs.
+
 app.use(router(updateUser).handler());
 
 export {app};
