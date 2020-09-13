62f1887e3bfedbd6769bf567fc0bbf857539c840
Sampsa Kuronen <sampsa.kuronen@gmail.com>
2020-09-13 10:16:29 +0300

    Add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import {router} from 'typera-express';
+import * as helmet from 'helmet';
 
 import {updateUser} from './routes/user';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(helmet({contentSecurityPolicy: false})); // TODO: Change the CSP to match your needs.
 app.use(router(updateUser).handler());
 
 export {app};
